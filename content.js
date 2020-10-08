519f1694bd06a73edf044f2f18a943cd064adf8e
Mehdi Rifi <mehdirifi51@gmail.com>
2020-10-08 23:34:36 +0100

    feat: enhance extract reg expression

--- content.js
+++ content.js
@@ -4,7 +4,7 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   switch (request) {
     // extract phone numbers
     case "extract":
-      let re = /\+(?:\(?\d{2,4}\)?)?\s?\(?0?\)?(?:\s?-?\d{1,3}){4}/gim;
+      let re = /\+\d{1,4}\s?\(?\d{1,2}\)?(?:\s?\-?\d{1,3}){3,5}/gim;
       result = document.documentElement.innerText.match(re);
       sendResponse(result);
       break;
